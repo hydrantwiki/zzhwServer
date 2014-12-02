b4a8ae39ddf50fd5040153ecdbccd5ed0ad48d21
Brian Nelson <brian@treegecko.com>
2014-12-02 05:27:03 +0000

    fixed

--- src/Site/Modules/AppModule.cs
+++ src/Site/Modules/AppModule.cs
@@ -56,6 +56,9 @@ namespace Site.Modules
 
                         return View["home.sshtml", userStats];
                     }
+
+                    userStats = new UserStats();
+                    return View["home.sshtml", userStats];
                 }
 
                 return null;
