d48b17c8a4fd89fc535ac98506abe379e01a730d
Brian Nelson <brian@treegecko.com>
2015-11-15 17:52:51 -0800

    mods

--- src/Site/Properties/AssemblyInfo.cs
+++ src/Site/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Site")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("HydrantWiki")]
+[assembly: AssemblyDescription("HydrantWiki Web Site")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Site")]
-[assembly: AssemblyCopyright("Copyright ©  2013")]
+[assembly: AssemblyCompany("Brian Nelson")]
+[assembly: AssemblyProduct("HydrantWiki")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
